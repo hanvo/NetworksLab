8b45f3d29118b5bede00dd2dd68341e757505074
phand <phand@purdue.edu>
2016-03-01 20:47:33 -0500

    Lab06 init

---
+++ lab06/net/packetdump.c
@@ -0,0 +1,16 @@
+/* packetdump.c - packetdump */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ * packetdump  -  Dumps contents of an Ethernet packet
+ *------------------------------------------------------------------------
+ */
+void	packetdump (
+		struct	netpacket *pptr	/* Pointer to a packet	*/
+		)
+{
+	/* Put your code here */
+
+	return;
+}
