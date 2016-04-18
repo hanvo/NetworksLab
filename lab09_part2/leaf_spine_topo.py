94766904067b6e9a7a208c066f7f6f60321ee3e1
phand <phand@purdue.edu>
2016-04-18 13:40:16 -0400

    Moved over the code that TA provided

--- lab09_part2/leaf_spine_topo.py
+++ lab09_part2/leaf_spine_topo.py
@@ -11,24 +11,36 @@ class   LeafAndSpineTopo (Topo):
     #    fanout - Number of hosts per leaf switch
     #
     def build(self, nSpine, nLeaf, fanout):
-    	listSpine = list()
-        listLeaf = list()
 
-        for spine in range(nSpine): 
-            switch = self.addSwitch('s%s' % (spine + 1))
-            listSpine.append(switch)
+    # Add spine switches, named spine1, spine2 ...
+        for s in range(1, nSpine+1):
 
-        for leaf in range(nLeaf):
-            leaf = self.addSwitch('l%s' % (leaf + 1))
-            listLeaf.append(leaf)
+            self.addSwitch('spine%s' % s)
 
-        for spine in listSpine:
-            for leaf in listLeaf:
-                self.addLink(spine, leaf)
+    # Counter for host name
+        i = 1
 
-        counter = 1
-        for leaf in listLeaf:
-            for host in range(fanout):
-                host = self.addHost('h%s' % counter)
-                self.addLink(host, leaf)
-                counter += 1
+    # Add leaf switches, named leaf1, leaf2 ...
+    # Add hosts, named h1, h2 ... such that
+    # h1, h2, ... hF are connected to leaf1,
+    # hF+1, hF+2, ... h2F are connected to leaf2,
+    # and so on ... (F = fanout)
+        for s in range(1, nLeaf+1):
+
+            # Add a leaf switch
+            self.addSwitch('leaf%s' % s)
+
+            # Connect to leaf switch to all the spine switches
+            for p in range(1, nSpine+1):
+
+                self.addLink('leaf%s' % s, 'spine%s' % p)
+
+            # Add fanout number of hosts and connect them to the
+            # leaf switch created above
+            for h in range(1, fanout+1):
+
+                self.addHost('h%s' % i)
+                self.addLink('leaf%s' % s, 'h%s' % i)
+
+                # Update the host name counter
+                i = i + 1
